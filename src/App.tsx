699d74ad0f60aacbc3b91af853b06ed36ae0c63c
daniel_benedik <sbhtkb129@gmail.com>
2025-08-25 14:53:40 +0300

    update App component header to display user name

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
 import React from "react";
 
 import styles from "./app.module.scss";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className={styles.container}>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit 1<code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">Daniel</header>
     </div>
   );
 }
